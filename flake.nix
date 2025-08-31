046480f79800f7c00d59b35a22708829ca81afe3
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-08-31 10:26:05 +0900

    use phases

--- flake.nix
+++ flake.nix
@@ -12,14 +12,17 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
-          package = pkgs.stdenv.mkDerivation {
-            name = "example";
+          example-project = pkgs.stdenv.mkDerivation {
+            pname = "example";
+            version = "0";
             src = ./.;
             buildInputs = with pkgs; [
               cabal-install
               ghc
             ];
-            buildCommand = ''
+            buildPhase = ''
+              runHook preBuild
+
               # `cabal build` writes a file at a local repository,
               # and so it must be writable.
               cp -r --no-preserve=all $src/.local-repository .
@@ -30,12 +33,39 @@
               cabal="cabal --project-dir=$src --builddir=$TMPDIR --verbose"
               $cabal v2-build --only-dependencies all
               $cabal v2-build all
-              $cabal v2-install --installdir=$out all
+
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              $cabal v2-install --installdir=$out --install-method=copy all
+              runHook postInstall
+            '';
+          };
+          download-dependencies = pkgs.stdenv.mkDerivation {
+            pname = "haskell-airplane-download-dependencies";
+            version = "0";
+            src = ./dev;
+            meta.mainProgram = "haskell-airplane-download-dependencies";
+            buildInputs = with pkgs; [
+              haskellPackages.cabal-plan
+              wget
+            ];
+            buildPhase = ''
+              runHook preBuild
+              cp $src/download-dependencies haskell-airplane-download-dependencies
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              install -D --target-directory=$out/bin haskell-airplane-download-dependencies
+              runHook postInstall
             '';
           };
         in
         {
-          packages.default = package;
+          packages.default = example-project;
+
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               haskell.compiler.ghc984
